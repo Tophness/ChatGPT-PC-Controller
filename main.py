6e5a2df00cc634ddb3cc868672576258d1f6c406
Chris Malone <chrismrulz@hotmail.com>
2023-06-24 02:24:07 +1000

    add direct mouse and keyboard control, only pass as many arguments as provided for all functions

--- main.py
+++ main.py
@@ -17,24 +17,37 @@ def execute_commands(cmd_string):
         args = shlex.split(cmd)
         print(f'Command: {args[0]}, Arguments: {args[1:]}')
         if args[0] == "run":
-            autoit.run(args[1])
+            autoit.run(*args[1:])
         elif args[0] == "win_wait_active":
-            if(len(args) > 2):
-                autoit.win_wait_active(args[1], int(args[2]))
-            else:
-                autoit.win_wait_active(args[1])
+            if len(args) > 2: args[2] = int(args[2])
+            autoit.win_wait_active(*args[1:])
         elif args[0] == "control_send":
-            autoit.control_send(args[1], args[2], args[3])
+            autoit.control_send(*args[1:])
         elif args[0] == "activate_window":
-            autoit.win_activate(args[1])
+            autoit.win_activate(*args[1:])
         elif args[0] == "maximize_window":
-            autoit.win_maximize(args[1])
+            autoit.win_maximize(*args[1:])
         elif args[0] == "minimize_window":
-            autoit.win_minimize(args[1])
+            autoit.win_minimize(*args[1:])
         elif args[0] == "move_window":
-            autoit.win_move(args[1], int(args[2]), int(args[3]))
+            if len(args) > 2: args[2] = int(args[2])
+            if len(args) > 3: args[3] = int(args[3])
+            autoit.win_move(*args[1:])
         elif args[0] == "close_window":
-            autoit.win_close(args[1])
+            autoit.win_close(*args[1:])
+        elif args[0] == "mouse_click":
+            if len(args) > 2: args[2] = int(args[2])
+            if len(args) > 3: args[3] = int(args[3])
+            if len(args) > 4: args[4] = int(args[4])
+            if len(args) > 5: args[5] = int(args[5])
+            autoit.mouse_click(*args[1:])
+        elif args[0] == "mouse_move":
+            if len(args) > 1: args[1] = int(args[1])
+            if len(args) > 2: args[2] = int(args[2])
+            if len(args) > 3: args[3] = int(args[3])
+            autoit.mouse_move(*args[1:])
+        elif args[0] == "keyboard_send":
+            autoit.send(*args[1:])
         else:
             print(f"Unknown command: {args[0]}")
 
