01ede3aadab4c8a2e1102a4addb18611d7cb244c
Chris Malone <chrismrulz@hotmail.com>
2023-06-28 16:23:24 +1000

    update prompt

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ config = None
 unattended = False
 preprompt = '''You are directly controlling a windows PC using autoit functions.
 I will reply back with the result of any commands that return a value and you can use that to decide what commands to generate next.
-Output only functions and nothing else. Don't set any variables. Don't use window handles. Don't comment anything.
+Output only the functions to run and nothing else. Don't set any variables. Don't use window handles. Don't comment anything.
 Here is an example to type hello world in notepad.
 `Run('notepad.exe')
 WinWaitActive('[CLASS:Notepad]','',3)
